f9364662508c08f342516e7485686a5c2e7088fb
duciker14 <duciker14@gmail.com>
2021-04-22 14:23:21 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,32 @@
+package basic.dev;
+
+public class MainApp {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Thong tin sinh vien*/
+		
+		String nameStudent;
+		String idStudent;
+		String cmndStudent;
+		String nameClassStu;
+		String nameSchoolStu;
+		float diemTrungBinh;
+		
+		/*Thong tin cong dan*/
+		
+		String namePerson;
+		String adressPerson;
+		String numberPhonePerson;
+		float heSoLuongPerson;
+		
+		/* Thong tin xe*/
+		
+		String nameCar;
+		String hangCar;
+		int giaCar;
+		String colorCar;
+
+	}
+
+}
